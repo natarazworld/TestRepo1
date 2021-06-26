ebf2fda648cdea828baf8a6a64b37374a2912d7d
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-06-26 22:20:01 +0530

    hello1

--- Proj1/src/com/nt/comp/MainApp1.java
+++ Proj1/src/com/nt/comp/MainApp1.java
@@ -5,7 +5,7 @@ public class MainApp1 {
 	public static void main(String[] args) {
 		System.out.println("sum ::"+new A().sum(10,20));
 		System.out.println("sub ::"+new B().sub(10,20));
-
+            //hello
 	}
 
 }
