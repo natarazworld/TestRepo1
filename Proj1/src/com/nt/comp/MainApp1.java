515e92e4d4dae9d098a07c4c84d99c53f1aaf929
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-06-26 22:16:08 +0530

    chanage2

---
+++ Proj1/src/com/nt/comp/MainApp1.java
@@ -0,0 +1,11 @@
+package com.nt.comp;
+
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		System.out.println("sum ::"+new A().sum(10,20));
+		System.out.println("sub ::"+new B().sub(10,20));
+
+	}
+
+}
